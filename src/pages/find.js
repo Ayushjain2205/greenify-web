15f7183da5cda9acff7b9351d93170aa27799d5c
Ayush Jain <ayushjain2205@gmail.com>
2023-04-15 03:19:36 +0530

    Add scrape call

--- src/pages/find.js
+++ src/pages/find.js
@@ -1,6 +1,23 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import Axios from "axios";
 
 const find = () => {
+  const [searchString, setSearchString] = useState([]);
+
+  const [data, setData] = useState([]);
+
+  const fetchData = async () => {
+    try {
+      const response = await Axios.get(
+        `api/scrape?searchString=${searchString}`
+      );
+      console.log(response.data);
+      setData(response.data);
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
   return (
     <div className="flex flex-col items-center align justify-center p-32">
       <h1 className="text-6xl text-secondary italic mb-16 font-bold">
@@ -11,8 +28,12 @@ const find = () => {
           type="text"
           placeholder="Type here"
           className="input input-bordered input-primary w-full"
+          onChange={(e) => setSearchString(e.target.value)}
         />
-        <button className="btn text-3xl text-primary btn-square btn-ghost">
+        <button
+          className="btn text-3xl text-primary btn-square btn-ghost"
+          onClick={() => fetchData()}
+        >
           <i class="fa-brands fa-searchengin"></i>
         </button>
       </div>
