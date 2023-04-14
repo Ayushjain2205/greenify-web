21fb0c18425546075fbdf44e0d14c4b1747efad8
Ayush Jain <ayushjain2205@gmail.com>
2023-04-15 00:28:34 +0530

    Add font awesome and lottie support

--- src/pages/_document.js
+++ src/pages/_document.js
@@ -4,9 +4,37 @@ export default function Document() {
   return (
     <Html data-theme="lemonade" lang="en">
       <Head />
+      <link
+        rel="stylesheet"
+        href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css"
+        integrity="sha512-iecdLmaskl7CVkqkXNQ/ZH/XLlvWZOJyj7Yy7tcenmpD1ypASozpmT/E0iPtmFIB46ZmdtAc9eNBvH0H/ZpiBw=="
+        crossorigin="anonymous"
+        referrerpolicy="no-referrer"
+      />
       <body>
-        <div className="navbar bg-base-100">
-          <a className="btn btn-ghost normal-case text-xl">daisyUI</a>
+        <div className="navbar bg-primary">
+          <div className="flex-1">
+            <a className="btn btn-secondary btn-outline normal-case text-xl">
+              <i class="fa-brands fa-envira"></i> &nbsp; Greenify Web
+            </a>
+          </div>
+          <div className="flex-none">
+            <button className="btn btn-square btn-ghost">
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                fill="none"
+                viewBox="0 0 24 24"
+                className="inline-block w-5 h-5 stroke-current"
+              >
+                <path
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  strokeWidth="2"
+                  d="M5 12h.01M12 12h.01M19 12h.01M6 12a1 1 0 11-2 0 1 1 0 012 0zm7 0a1 1 0 11-2 0 1 1 0 012 0zm7 0a1 1 0 11-2 0 1 1 0 012 0z"
+                ></path>
+              </svg>
+            </button>
+          </div>
         </div>
         <Main />
         <NextScript />
