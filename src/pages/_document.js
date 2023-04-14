ac68f24e6f85867a6a47a636e2dbffaa8dde6bcc
Ayush Jain <ayushjain2205@gmail.com>
2023-04-14 23:13:31 +0530

    Init project

--- src/pages/_document.js
+++ src/pages/_document.js
@@ -1,13 +1,16 @@
-import { Html, Head, Main, NextScript } from 'next/document'
+import { Html, Head, Main, NextScript } from "next/document";
 
 export default function Document() {
   return (
-    <Html lang="en">
+    <Html data-theme="lemonade" lang="en">
       <Head />
       <body>
+        <div className="navbar bg-base-100">
+          <a className="btn btn-ghost normal-case text-xl">daisyUI</a>
+        </div>
         <Main />
         <NextScript />
       </body>
     </Html>
-  )
+  );
 }
